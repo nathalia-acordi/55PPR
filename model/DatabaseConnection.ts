c617f9ea3ed880ef48837f62743cbc52bd3aab9b
Nicolas Weber <nicolasweber0901@outlook.com>
2024-01-21 20:05:22 -0300

    Alterações database connection

--- model/DatabaseConnection.ts
+++ model/DatabaseConnection.ts
@@ -1,19 +1,40 @@
+import { Cliente } from "./Cliente";
+import { Livro } from "./Livro";
+
 class DatabaseConnection {
-    private static instance: DatabaseConnection;
-    private connection: any;
-   
-    private constructor() {
-      // Inicializa a conexão com o banco de dados
+  private static instance: DatabaseConnection;
+  private clientes: Cliente[];
+  private livros: Livro[];
+  private emprestimos: Emprestimo[];
+
+  constructor() {
+    this.clientes = [];
+    this.livros = [];
+    this.emprestimos = [];
+  }
+
+  public static getInstance(): DatabaseConnection {
+    if (!DatabaseConnection.instance) {
+      DatabaseConnection.instance = new DatabaseConnection();
     }
-   
-    public static getInstance(): DatabaseConnection {
-      if (!DatabaseConnection.instance) {
-        DatabaseConnection.instance = new DatabaseConnection();
-      }
-      return DatabaseConnection.instance;
+    return DatabaseConnection.instance;
+  }
+
+  adicionarCliente(cliente: Cliente): void {
+    this.clientes.push(cliente);
+  }
+
+  removerCliente(cliente: Cliente): void {
+    const index = this.clientes.indexOf(cliente);
+    if (index > -1) {
+      this.clientes.splice(index, 1);
     }
-   
-    public getConnection(): any {
-      return this.connection;
+  }
+
+  editarCliente(clienteAtualizado: Cliente): void {
+    const index = this.clientes.indexOf(clienteAtualizado);
+    if (index > -1) {
+      this.clientes[index] = clienteAtualizado;
     }
-   }
+  }
+}
